9a4cb1c466727c7a3bdf2105e81ec06da8a09a69
UladKasach <nappa300@gmail.com>
2018-02-15 13:54:58 -0500

    update default promise properties and clientsiderequire settings in package

--- src/options_functions.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    load : function(path){
-        var load_promise = this.then((view_loader)=>{ return view_loader.load(path)}) // define `view_loader.load()` to the view_loader promise
-        load_promise.generate = function(options){ return this.then((compiler)=>{return compiler.generate(options)})}; // define view_loader.load().generate() to the promise
-        return load_promise;
-    }, 
-}
