b0dc4a8f5bdacf0ce7a2ee214d616b92b11153e5
UladKasach <nappa300@gmail.com>
2018-02-15 14:51:48 -0500

    remove require default properties

--- src/default_require_properties.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    load : function(path){
-        return this.then((view_loader)=>{ return view_loader.load(path)});
-    },
-    generate : function(options){
-        return this.then((compiler)=>{return compiler.generate(options)});
-    }
-}
